d46bb0d6ddf95455d3442034802c83ba2e4b133e
GitMulC <cmulvo@gmail.com>
2024-08-04 18:41:07 +0100

    Remove selection functionality for starters

--- src/components/Starters_cards.js
+++ src/components/Starters_cards.js
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Button, Row, Col, Card } from 'react-bootstrap';
 
-function Startes_cards() {
+function Starters_cards() {
     const [starters, setStarters] = useState([]);
     const [error, setError] = useState(null);
-    const [selectedStarter, setSelection] = useState(null);
 
     useEffect(() => {
         try {
@@ -22,11 +21,6 @@ function Startes_cards() {
 
     }, []);
 
-    function handleSelection(e) {
-        e.preventDefault();
-        setSelection('Selected');
-    }
-
     return (
         <div>
             <Row>
@@ -34,7 +28,6 @@ function Startes_cards() {
                     starters.map(function (i, index) {
                         return (
                             <Col key={index}>
-                                {selectedStarter && <div className="winechoice">Added {i.strMeal} to your menu</div>}
                                 <Card className="starter-card text-center mt-2 mb-2" style={{ width: '18rem' }}>
                                     <Card.Body>
                                         <Card.Img variant="top" src={i.strMealThumb} />
@@ -42,9 +35,6 @@ function Startes_cards() {
                                         <Card.Text>
                                         {error && <div className="alert">{error}</div>}
                                         </Card.Text>
-                                        <Button className="button" onClick={handleSelection}>
-                                            Select
-                                        </Button>
                                         <a href={`https://www.themealdb.com/meal/${i.idMeal}`} target='_blank'>
                                             <Button className="button">
                                                 Try it Yourself!
@@ -61,4 +51,4 @@ function Startes_cards() {
     );
 }
 
-export default Startes_cards
+export default Starters_cards
