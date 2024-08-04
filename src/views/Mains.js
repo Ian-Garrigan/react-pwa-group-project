778cefbd022137065cfcab4624be30b63e935a16
GitMulC <cmulvo@gmail.com>
2024-08-04 22:12:05 +0100

    Add aditional btn for free flowing navigation

--- src/views/Mains.js
+++ src/views/Mains.js
@@ -1,11 +1,30 @@
 /*Filename:Mains.js*/
+//Author: Conor Mulvihill
+//Date 4th August 2024
 
+import { Row, Col, Button } from 'react-bootstrap';
 import Mains_cards from '../components/Mains_cards';
+import { Link } from "react-router-dom";
 
 function Mains() {
 
   return (
-    <Mains_cards />
+    <div>
+      <h2 className='heading text-underlined text-center'>Our Mains & Sides</h2>
+      <Mains_cards />
+      <br></br>
+      <br></br>
+      <Row className="text-center">
+        <Col>  
+        <Link to="/Desserts">
+          <Button className="button">See Desserts</Button>
+          </Link>
+          <br></br>
+          <br></br>
+        </Col>
+      </Row>
+    </div>
+    
   )
 };
 
--- src/views/Starters.js
+++ src/views/Starters.js
@@ -1,7 +1,10 @@
 /*Filename:Starters.js*/
+//Author: Conor Mulvihill
+//Date 4th August 2024
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import Starters_cards from '../components/Starters_cards';
+import { Link } from "react-router-dom";
 
 function Starters() {
 
@@ -11,9 +14,19 @@ function Starters() {
       <Row>
         <Col lg={3}></Col>
         <Col lg={6}>
+        <p className='heading text-underlined text-center'>Our Starters</p>
           <Starters_cards />
         </Col>
         <Col lg={3}></Col>
+      </Row>
+      <Row className="text-center">
+        <Col>  
+        <Link to="/Mains">
+          <Button className="button">See Mains</Button>
+          </Link>
+          <br></br>
+          <br></br>
+        </Col>
       </Row>
     </Container>
   )
